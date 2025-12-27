4d5e02fe50f3bee4ddf6fb3700220b21f192523a
Evan Smith <12199891+e13h@users.noreply.github.com>
2025-12-27 16:48:53 -0700

    Update metadata files

--- onelineaday/export.sh
+++ onelineaday/export.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
+ONELINEADAY_PORT=3800
 OUTPUT_FILENAME="onelineaday_export_$(date -Is).json"
-(
-	docker exec \
-		-e PGPASSWORD=your_secret_password \
-		onelineaday-db-1 \
-		psql \
-			-U journal \
-			-d journal_db \
-			-t \
-			-A \
-			-c "SELECT json_agg(row_to_json(t)) FROM (SELECT date, message FROM journal_entries) t;" \
-	| jq
-) > $OUTPUT_FILENAME
+wget http://localhost:${ONELINEADAY_PORT}/api/export -O $OUTPUT_FILENAME
 echo $OUTPUT_FILENAME
